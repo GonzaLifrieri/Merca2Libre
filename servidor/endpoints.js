faba0c0333bbd1ec9ca76913297618e375377833
Lucas <lukymassaro@gmail.com>
2020-03-05 21:34:07 -0300

    config puerto

--- servidor/endpoints.js
+++ servidor/endpoints.js
@@ -19,3 +19,11 @@ app.get('/usuario/:id/actualizar', function(req, res){
 app.get('/usuarios/list', function(req, res){
     console.log('Ruta para crear usuarios');
 })
+
+
+//seteamos el puerto en el cual va a escuchar los pedidos la aplicación
+var puerto = '8080';
+
+app.listen(puerto, function () {
+  console.log( "Escuchando en el puerto " + puerto );
+});
