b66b7ecfa1c36e8c418bfef51fd2a08b4e359c15
Lucas <lukymassaro@gmail.com>
2020-03-10 21:04:16 -0300

    Controlador para crear nueva tienda

--- servidor/endpoints.js
+++ servidor/endpoints.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const controladorUsuario = require('../controladores/controladorUsuario');
+const controladorTienda = require('../controladores/controladorTienda');
 
 //config de seguridad -- son todos middleware
 const app = express();
@@ -18,6 +19,8 @@ app.get('/usuario/:id/actualizar', function(req, res){
     console.log('Ruta para actualizar usuarios');
 });
 app.get('/usuarios/list', controladorUsuario.usuariosList);
+
+app.get('/tienda/crear', controladorTienda.crear);
 
 
 //seteamos el puerto en el cual va a escuchar los pedidos la aplicación
