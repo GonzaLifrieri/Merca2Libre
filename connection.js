5c5a242a22c42a94bd849bf8521e5802c75edc11
Lucas <lukymassaro@gmail.com>
2020-03-26 19:25:15 -0300

    Agregar funcion de actualizar en controladorProducto

--- connection.js
+++ connection.js
@@ -2,10 +2,6 @@ const mysql = require('mysql');
 const node_env = require('./env.json');
 
 var connection = mysql.createConnection({
-<<<<<<< HEAD
-=======
-
->>>>>>> c3f4ea90839bebfd7ef0ce10b7e1d5db4ca2b73b
     host: node_env.database.DATABASE_URL,
     port: node_env.database.DATABASE_PORT,
     user: node_env.database.DATABASE_USER,
